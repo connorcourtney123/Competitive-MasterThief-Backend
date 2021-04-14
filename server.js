c0af12a51f3a4f913c11d58e430b3505bd4760ac
ConnorCourtney <connorcourtney123@gmail.com>
2021-04-13 21:51:14 -0400

    last minute fixes

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const {default: axios} = require('axios')
 var cors = require('cors')
 app.use(cors())
 
-//listen on port 3000
-app.listen(3000, () => {
-  console.log('the server is listening!')
-	routesReport.print()
-})
+// //listen on port 3000
+// app.listen(3000, () => {
+//   console.log('the server is listening!')
+// 	routesReport.print()
+// })
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('server listening on ${PORT}');
@@ -36,7 +36,7 @@ const saveScore = async (req,res) => {
         var changedUser = {}
 
         //compare scores
-        if(user.score > req.params.score){
+        if(user.score < req.params.score){
             //give user new score if better
             let changedUser = await user.update({
                 score: req.params.score,
@@ -64,3 +64,123 @@ const saveScore = async (req,res) => {
 
 //route
 app.post('/user/:id/win/:score', saveScore)
+
+//login
+//controller:
+const getUserByUsername = async (req, res) => {
+    try{
+        let user = await models.user.findOne({
+            where: {
+                userName: req.body.username
+            }
+        })
+
+        res.json({user})
+
+
+    }catch(error){
+        console.log(error);
+        res.json({error})
+    }
+}
+
+//route:
+app.post('/login', getUserByUsername)
+
+// sign up
+
+//controller
+const createUser = async (req, res) => {
+    try{
+        let newUser = await models.user.create({
+            name: req.body.name,
+            userName: req.body.username,
+            password: req.body.password,
+            score: 0,
+            wins: 0,
+            losses: 0
+        })
+
+
+        res.json({newUser})
+
+    }catch(error){
+        console.log(error)
+        res.json({error})
+    }
+}
+
+
+//route
+app.post('/signup', createUser)
+
+//addfriend
+//controller
+const addFriend = async (req, res) => {
+    try{
+        console.log(req.body.username)
+
+        let friend = await models.user.findOne({
+            where: {
+                userName: req.body.username
+            }
+        })
+
+        let user = await models.user.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        let newFriendship = await models.friendship.create({
+            userId: req.params.id,
+            friendId: friend.id
+        })
+
+        await user.addFriendship(newFriendship)
+
+        res.json({user, friend:friend, newFriendship:newFriendship})
+
+    }catch(error){
+        res.json({error})
+    }
+}
+
+//route
+app.post('/user/:id/addFriend', addFriend)
+
+//get all friends
+//controller
+const getFriends = async (req, res) => {
+    try{
+        let friendships = await models.friendship.findAll({
+            where: {
+                userId: req.params.id
+            }
+        })
+
+        let friendIds = []
+
+        for(friendship of friendships){
+            friendIds.push(friendship.friendId)
+        }
+
+        let friends = []
+
+        for(id of friendIds){
+            let friend = await models.user.findOne({
+                where: {
+                    id: id
+                }
+            })
+            friends.push(friend)
+        }
+
+        res.json({friends})
+    }catch(error){
+        console.log(error)
+        res.json({error})
+    }
+}
+//route
+app.get('/user/:id/friends', getFriends)
